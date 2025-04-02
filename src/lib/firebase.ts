f0da575bc08b0f9868b66de3ff558b41d35a8ee4
Wen Qi Kho <shaankho1998@gmail.com>
2025-04-02 10:27:41 +0800

    Initialized Firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFVX9la0UMG6RAfGLpOMZ-agOtf4E9ikM",
+  authDomain: "makanplus-e9139.firebaseapp.com",
+  projectId: "makanplus-e9139",
+  storageBucket: "makanplus-e9139.firebasestorage.app",
+  messagingSenderId: "844676094868",
+  appId: "1:844676094868:web:af76ee096a75d406c48d81"
+};
+
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+
+export { app, db, firebaseConfig };
