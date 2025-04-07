ba23459c3ee56ed6face4670cb7295be0f14d991
Wen Qi Kho <shaankho1998@gmail.com>
2025-04-07 11:35:48 +0800

    feat: add immediate feedback and error handling for Gemini API call
    - Implement try-catch to simplify testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,38 +12,52 @@ async function generateContent(contents: string): Promise<string> {
     contents,
   });
   
-  return response.text ?? "No response returned";
+  return response.text ?? "No response returned"
 }
 
 export default function Home() {
-  const [inputText, setInputText] = useState('');
-  const [outputText, setOutputText] = useState('');
+  const [inputText, setInputText] = useState('')
+  const [outputText, setOutputText] = useState('')
+  const [isToggled, setIsToggled] = useState(false)
 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setInputText(e.target.value);
-  };
+    setInputText(e.target.value)
+  }
 
   const handleSubmit = async () => {
-    
-    const text = await generateContent(inputText);
-    setOutputText(text);
-  };
+
+    setOutputText("thinking...")
+    try {
+      const text = await generateContent(inputText)
+      setOutputText(text)
+    } catch (e) {
+      setOutputText("Sorry, something went wrong. Please try again.")
+    }
+  }
 
   return (
     <>
-      <div className="field-wrap">
-        <label>Gemini test:</label>
-        <input
-          type="text"
-          value={inputText}
-          onChange={handleInput}
-          placeholder="ask any question here"
-          className="border-b border-solid border-white-300"
-        />
-      </div>
-      
-      <button onClick={handleSubmit} className="btn mt-4">Submit</button>
-      <span>Gemini says:</span><div>{outputText}</div>
+      <h1>This is home page</h1>
+
+
+      <button onClick={() => setIsToggled((prev) => !prev)}>Test</button>
+      {isToggled &&
+        <div>
+          <div className="field-wrap">
+            <label>Gemini test:</label>
+            <input
+              type="text"
+              value={inputText}
+              onChange={handleInput}
+              placeholder="ask any question here"
+              className="border-b border-solid border-white-300"
+            />
+          </div>
+          
+          <button onClick={handleSubmit} className="btn mt-4">Submit</button>
+          <span>Gemini says:</span><div>{outputText}</div>
+        </div>
+      }
     </>
   );
 }
